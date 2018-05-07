37b03b459d473c5f9b8a8f3ca82b6ad39a56b378
stefan1995mm <dominik.stefanczyk@gmail.com>
2018-05-07 23:49:50 +0200

    Last task from study

--- src/java_uczelnia/swing/Przyciski.java
+++ src/java_uczelnia/swing/Przyciski.java
@@ -5,12 +5,35 @@
  */
 package java_uczelnia.swing;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 /**
  *
  * @author Stefan
  */
 public class Przyciski {
     public static void main(String[] args){
-        System.out.println("Tst");
+        UIManager.LookAndFeelInfo[] style = UIManager.getInstalledLookAndFeels();
+        for(UIManager.LookAndFeelInfo styl: style){
+            System.out.println(styl);
+        }
+       
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Ramka ramka1 = new Ramka();
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                } catch (Exception e) {
+                  e.printStackTrace();
+                }
+                Ramka ramka2 = new Ramka();
+                ramka1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                ramka1.setVisible(true);
+                ramka2.setVisible(true);
+            }
+        });
     }
 }
---
+++ src/java_uczelnia/swing/Ramka.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_uczelnia.swing;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author Stefan
+ */
+
+public class Ramka extends JFrame{
+    private static int szerokosc = 600;
+    private static int wysokosc = 400;
+    private static final String TYTUL = "Edytor";
+    private final int szerokoscEkranu;
+    private final int wysokoscEkranu;
+    private JPanel panel;
+    private JLabel etykieta;
+    private JButton przycisk;
+    private JTextField tekst;
+    public Ramka(){
+        setSize(szerokosc,wysokosc);
+        setLocationByPlatform(true);
+        setTitle(TYTUL);
+        setResizable(false);
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension rozmiarEkranu = tk.getScreenSize();
+        szerokoscEkranu = rozmiarEkranu.width;
+        wysokoscEkranu = rozmiarEkranu.height;
+        szerokosc = szerokoscEkranu/2;
+        wysokosc = wysokoscEkranu/2;
+        setSize(szerokosc,wysokosc);
+        panel = new JPanel();
+        etykieta = new JLabel();
+        etykieta.setText("Przed zmianą");
+        panel.add(etykieta);
+        przycisk=new JButton("zmiana etykiety");
+        tekst = new JTextField();
+        tekst.setEnabled(true);
+        tekst.setColumns(10);
+        przycisk.addActionListener((ActionEvent e) -> {
+            etykieta.setText("Po zmianie");
+            if(tekst.isEnabled()){
+                tekst.setEnabled(false);
+            }
+            else{
+                tekst.setEnabled(true);
+            }
+        });
+        panel.add(przycisk);
+        panel.add(tekst);
+        add(panel);
+    }
+}
+/*
+        przycisk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               etykieta.setText("Po zmianie");
+               if(tekst.isEnabled()){
+                   tekst.setEnabled(false);
+               }
+               else{
+                   tekst.setEnabled(true);
+               }
+            }
+        });*/
